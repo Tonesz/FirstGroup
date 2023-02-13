97beb297a3d1b67a85b5672296f9afe2dc40d6ae
Hunan <safaryan.hunan@gmail.com>
2023-02-13 16:46:33 +0100

    idk

--- GitAdvanced/src/Main.java
+++ GitAdvanced/src/Main.java
@@ -4,21 +4,13 @@ public class Main {
 
     }
 
-    public void future(String future1, String future2) {
-        System.out.println(future1 + ", " + future2);
-        future("Scorpio", "cancer");
+    public void future(String future2) {
+        System.out.println(future2);
+
     }
-<<<<<<< HEAD
-    public static void twoParam(String first, String second) {
-        System.out.println("First param: " + first);
-        System.out.println("Second param: " + second);
-=======
-
-    
-
     public static void count(String parameter) {
         System.out.println("Counting the countless " + parameter);
->>>>>>> 950fcb4471d4781d721835bed1c568350cc27849
+
     }
 
     public static void greet(String parameter) {

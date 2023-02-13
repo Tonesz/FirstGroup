c08113116a414ea74234cdc19ae6c0c04d014576
Hunan <safaryan.hunan@gmail.com>
2023-02-13 14:30:07 +0100

    a5

--- GitAdvanced/src/Main.java
+++ GitAdvanced/src/Main.java
@@ -2,4 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public static void greet(String name, String lastName) {
+        System.out.println(name + lastName);
+    }
 }

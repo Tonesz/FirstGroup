0fcbd133c89cf8b43b62ea1ba1fabdb18c92c587
LuMlyna <lu.mlynarcikova@gmail.com>
2023-02-13 14:40:07 +0100

    d5

--- GitAdvanced/src/Main.java
+++ GitAdvanced/src/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public void future(String future1, String future2){
+        System.out.println(future1 + ", " + future2);
+        future("Scorpio", "cancer");
+    }
 }

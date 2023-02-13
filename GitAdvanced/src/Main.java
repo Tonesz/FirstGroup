ea346122622ff0ace630e87ff651a8206bf934cd
Natesz <natesz777@gmail.com>
2023-02-13 14:35:52 +0100

    b5

--- GitAdvanced/src/Main.java
+++ GitAdvanced/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+
+    public static void twoParam(String first, String second) {
+        System.out.println("First param: " + first);
+        System.out.println("Second param: " + second);
     }
 }
